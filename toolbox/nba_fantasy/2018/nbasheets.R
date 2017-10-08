8d2efd1696324722a20fbdc9e780e378083a30c5
Clement Fung <clement.y.fung@gmail.com>
2017-10-07 23:59:59 -0700

    Adding 2018 nba data

--- toolbox/nba_fantasy/2018/nbasheets.R
+++ toolbox/nba_fantasy/2018/nbasheets.R
@@ -5,28 +5,37 @@ getZdf <- function(df) {
   n <- dim(df)[1]
   d <- dim(df)[2]
 
-  stats <- colnames(df)[6:d]
+  # Indices of the first stat, and the last. Should only include 9-cat
+  fs <- 6
+  ls <- 14 
 
-  zdf <- data.frame(matrix(nrow=n,ncol=(d+2)))
+  stats <- colnames(df)[fs:ls]
+
+  # We will add the Z_AVG column
+  zdf <- data.frame(matrix(nrow=n, ncol=(ls+1)))
   colnames(zdf) = colnames(df)
-  colnames(zdf)[6:d] = paste("Z", stats, sep ="_")
-  colnames(zdf)[d+1] = paste("Z_AVG")
-  colnames(zdf)[d+2] = paste("Tier")
+  colnames(zdf)[fs:ls] = paste("Z", stats, sep ="_")
+  colnames(zdf)[ls+1] = paste("Z_AVG")
+  #colnames(zdf)[ls+2] = paste("Tier")
 
-  zdf[,1:5] <- df[,1:5]
-  for (j in 6:(d-1)) {
+  # Copy over all the non-stat columns
+  zdf[,1:(fs-1)] <- df[,1:(fs-1)]
+
+  # Compute the z-score for each stat
+  for (j in fs:ls) {
     zdf[,j] <- (df[,j] - mean(df[,j])) / sd(df[,j])
   }
 
   # Reverse the zscore for TO
-  zdf[,d] <- (mean(df[,d]) - df[,d]) / sd(df[,d])
+  zdf[,ls] <- (mean(df[,ls]) - df[,ls]) / sd(df[,ls])
 
+  # Add the Z_AVG column
   for (i in 1:n){
-    zdf[i,(d+1)] = mean(as.numeric(zdf[i,6:d]))
+    zdf[i,(ls+1)] = mean(as.numeric(zdf[i,fs:ls]))
   }
 
   # Round to 3 places
-  zdf[,6:(d+1)] <- round(zdf[,6:(d+1)],3)
+  zdf[,fs:(ls+1)] <- round(zdf[,fs:(ls+1)],3)
 
   # Sort by average zscore
   zdf <- zdf[order(zdf$Z_AVG, decreasing = TRUE),]
@@ -49,7 +58,7 @@ addTiers <- function(df, tiers) {
 } 
 
 # START
-df <- read.csv("rawstats.csv", header=TRUE)
+df <- read.csv("2018raw.csv", header=TRUE)
 zdf <- getZdf(df)
 
 pg_zdf <- getZdf(df[grep("PG",df[,2]),])
@@ -58,20 +67,20 @@ sf_zdf <- getZdf(df[grep("SF",df[,2]),])
 pf_zdf <- getZdf(df[grep("PF",df[,2]),])
 c_zdf <- getZdf(df[grep("C",df[,2]),])
 
-pg_tiers <- read.csv("pg_tiers.csv", header=FALSE)
-pg_zdf <- addTiers(pg_zdf,pg_tiers)
+#pg_tiers <- read.csv("pg_tiers.csv", header=FALSE)
+#pg_zdf <- addTiers(pg_zdf,pg_tiers)
 
-sg_tiers <- read.csv("sg_tiers.csv", header=FALSE)
-sg_zdf <- addTiers(sg_zdf,sg_tiers)
+#sg_tiers <- read.csv("sg_tiers.csv", header=FALSE)
+#sg_zdf <- addTiers(sg_zdf,sg_tiers)
 
-sf_tiers <- read.csv("sf_tiers.csv", header=FALSE)
-sf_zdf <- addTiers(sf_zdf,sf_tiers)
+#sf_tiers <- read.csv("sf_tiers.csv", header=FALSE)
+#sf_zdf <- addTiers(sf_zdf,sf_tiers)
 
-pf_tiers <- read.csv("pf_tiers.csv", header=FALSE)
-pf_zdf <- addTiers(pf_zdf,pf_tiers)
+#pf_tiers <- read.csv("pf_tiers.csv", header=FALSE)
+#pf_zdf <- addTiers(pf_zdf,pf_tiers)
 
-c_tiers <- read.csv("c_tiers.csv", header=FALSE)
-c_zdf <- addTiers(c_zdf,c_tiers)
+#c_tiers <- read.csv("c_tiers.csv", header=FALSE)
+#c_zdf <- addTiers(c_zdf,c_tiers)
 
 write.csv(zdf, "./zdf.csv", quote=FALSE, row.names=FALSE)
 write.csv(pg_zdf, "./pg_zdf.csv",  quote=FALSE, row.names=FALSE)
